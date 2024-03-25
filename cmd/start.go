05b60a0834e25c6ddfefdb35a6c5e4fdd2ad83ac
lidenger <liya3@xiaomi.com>
2024-03-25 14:54:24 +0800

    feat: app.key

--- cmd/start.go
+++ cmd/start.go
@@ -10,8 +10,8 @@ import (
 )
 
 // CodeLevelProtectKey 提供代码级保护
-// 注意：即使对KEY文件提供了保护并不意味KEY文件可以被泄露
-// 由于代码是开源的所以是获取到的，所以KEY文件依然是高敏感的
+// 注意：即使对[app.key]文件提供了保护并不意味[app.key]文件可以被泄露
+// 由于代码是开源的所以是可以获取到的，因此[app.key]文件依然是高敏感的
 var CodeLevelProtectKey = []byte("0d3d2206ea5711ee87ba2cf05daf3fe5")
 var CodeLevelProtectIV = []byte("ad5457f8ea5711ee")
 
@@ -66,9 +66,9 @@ func GenKeyFile() {
 	keyFile := "app.key"
 	_, err = os.Stat(keyFile)
 	if err == nil {
-		panic(fmt.Sprintf("系统启动KEY文件已存在:%s", keyFile))
+		panic(fmt.Sprintf("系统启动[app.key]文件已存在:%s", keyFile))
 	}
-	err = os.WriteFile(keyFile, []byte(cipher), 0x777)
+	err = os.WriteFile(keyFile, []byte(cipher), 0x600)
 	if err != nil {
 		panic(err)
 	}
@@ -77,24 +77,24 @@ func GenKeyFile() {
 func AnalysisKeyFile(keyFile string) *Crypt {
 	cipher, err := os.ReadFile(keyFile)
 	if err != nil {
-		fmt.Printf("系统启动KEY文件不正确(读取文件失败):%+v", err)
+		fmt.Printf("系统启动[app.key]文件不正确(读取文件失败):%+v", err)
 		panic(err)
 	}
 	content, err := crypt.Decrypt(CodeLevelProtectKey, CodeLevelProtectIV, string(cipher))
 	if err != nil {
-		fmt.Printf("系统启动KEY文件不正确(解密失败):%+v", err)
+		fmt.Printf("系统启动[app.key]文件不正确(解密失败):%+v", err)
 		panic(err)
 	}
 	crypto := &Crypt{}
 	err = json.Unmarshal(content, crypto)
 	if err != nil {
-		fmt.Printf("系统启动KEY文件不正确(反序列化失败):%+v", err)
+		fmt.Printf("系统启动[app.key]文件不正确(反序列化失败):%+v", err)
 		panic(err)
 	}
 	// 对比数据摘要
 	digest := crypt.HmacDigest(CodeLevelProtectKey, crypto.RootKey+crypto.IV)
 	if digest != crypto.DataCheck {
-		panic("系统启动KEY文件不正确(数据校验不通过)")
+		panic("系统启动[app.key]文件不正确(数据校验不通过)")
 	}
 	return crypto
 }
