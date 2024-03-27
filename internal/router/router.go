24c509d278273a10ac3c8f21d555ae2bd1606107
lidenger <liya3@xiaomi.com>
2024-03-27 19:49:14 +0800

    clearup

--- internal/router/router.go
+++ internal/router/router.go
@@ -21,20 +21,22 @@ func api(g *gin.Engine) {
 
 	v1 := g.Group("/v1")
 	{
-		{
-			v1.GET("/valid", handler.ValidCode) // 验证动态令牌
-		}
-
 		secretv1 := v1.Group("/secret") // 账号密钥
+		secretv1.Use(middleware.ServerAuth)
 		{
+			secretv1.GET("/valid", handler.ValidCode)          // 验证动态令牌
 			secretv1.POST("", handler.AddAccountSecret)        // POST /v1/secret
 			secretv1.GET(":account", handler.GetAccountSecret) // GET /v1/secret/zhangsan
 		}
 
-		serverv1 := v1.Group("/server") // 接入服务
+		adminv1 := v1.Group("/admin") // 管理平台
+		adminv1.Use(middleware.AdminAuth)
 		{
-			serverv1.POST("", handler.AddServer)
-			serverv1.GET(":sign", handler.GetServer)
+			serverv1 := adminv1.Group("/server") // 接入服务
+			{
+				serverv1.POST("", handler.AddServer)
+				serverv1.GET(":sign", handler.GetServer)
+			}
 		}
 	}
 
