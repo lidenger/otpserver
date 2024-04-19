b874780b8ae9a4a91140623c58af64fb592cb79c
lidenger <liya3@xiaomi.com>
2024-04-19 10:38:21 +0800

    conf: local store test file detection

--- config/storeconf/localconf/local_conf.go
+++ config/storeconf/localconf/local_conf.go
@@ -3,6 +3,7 @@ package localconf
 import (
 	"fmt"
 	"github.com/lidenger/otpserver/config"
+	"github.com/lidenger/otpserver/config/log"
 	"github.com/lidenger/otpserver/pkg/enum"
 	"github.com/lidenger/otpserver/pkg/util"
 	"os"
@@ -23,11 +24,11 @@ func Initialize(conf *config.M) {
 		panic(err)
 	}
 	LocalConfIns.testFilePath = dirPath + "test_file_store.txt"
-	isExist, err := util.IsExistsFile(LocalConfIns.testFilePath)
+	isNotExist, err := util.IsNotExistsFile(LocalConfIns.testFilePath)
 	if err != nil {
 		panic(err)
 	}
-	if !isExist {
+	if isNotExist {
 		f, err := os.Create(LocalConfIns.testFilePath)
 		if err != nil {
 			panic(err)
@@ -41,11 +42,28 @@ func (l *LocalConf) GetStoreType() string {
 }
 
 func (l *LocalConf) CloseStore() {
-
+	log.Info("本地存储已关闭")
 }
 
 func (l *LocalConf) TestStore() error {
 	content := fmt.Sprintf("Local store检测时间: " + time.Now().Format(time.DateTime) + "\n")
+	isExist, err := util.IsExistsFile(l.testFilePath)
+	if err != nil {
+		return err
+	}
+	if isExist {
+		fi, err := os.Stat(l.testFilePath)
+		if err != nil {
+			return err
+		}
+		// 超过10MB删除
+		if fi.Size() > 10*1024*1024 {
+			err = os.Remove(l.testFilePath)
+			if err != nil {
+				return err
+			}
+		}
+	}
 	file, err := os.OpenFile(l.testFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
 	if err != nil {
 		return err
